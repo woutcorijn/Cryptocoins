4a000e2f02b810dc253c0cf5115d71c822f4958c
woutcorijn <wout.corijn@hotmail.com>
2018-03-25 22:45:34 +0200

    V1.0

---
+++ js.js
@@ -0,0 +1,39 @@
+var name;
+var price = 0;
+var money = 1000;
+var coin = 0;
+var money = 1000;
+
+
+function nameupdate() {
+	document.getElementById("money").innerHTML = "money: " + money + "$";
+    name = document.getElementById("coinNameinp").value;
+    document.getElementById("coinname").innerHTML = name + "coin";
+	document.getElementById("coin").innerHTML = name + "coins: " + coin;
+	document.getElementById("game").style.display = "block";
+	document.getElementById("setup").style.display = "none";
+}
+
+setInterval(function(){ 
+price = Math.floor((Math.random() * 10000) + 0);
+document.getElementById("value").innerHTML = "1 " + name + "coin = " + price + "$";
+}, 2000);
+
+function buy() {
+	coin += 1;
+	money -= price;
+	document.getElementById("money").innerHTML = "money: " + money + "$";
+	document.getElementById("coin").innerHTML = name + "coins: " + coin;
+}
+
+function sell() {
+	if (coin <= 0) {
+	money = money;	
+	coin =  0;
+	} else {
+	money += price;
+	coin -= 1;
+	}
+	document.getElementById("money").innerHTML = "money: " + money + "$";
+	document.getElementById("coin").innerHTML = name + "coins: " + coin;
+}
